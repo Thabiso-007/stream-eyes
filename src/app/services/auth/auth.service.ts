9656c38e664f744599120bb5a3ad0f10f533355a
Shalatiey23 <vchauke357@gmail.com>
2024-11-26 22:10:22 +0200

    calling backend api into frontend service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private API_URL = 'http://localhost:8645/api/v1';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  login(email:string, password:string) {
+    return this.http.post(`${this.API_URL}/login`, {email, password})
+     .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage
+  }
 }
